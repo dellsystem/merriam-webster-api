5dfc857fa0879c27cc567dacf8d5764fee2604ac
dellsystem <ilostwaldo@gmail.com>
2017-06-07 16:14:01 -0400

    Add functioning setup.py
    Necessary for installing with pip via a Github URL

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='Merriam-Webster API',
+    version='1.0',
+    description='A wrapper around the Merriam-Webster APIs.',
+    author='Paul Feitzinger',
+    author_email='paul@pfeyz.com',
+    url='https://github.com/pfeyz/merriam-webster-api',
+    packages=['merriam_webster'],
+)
